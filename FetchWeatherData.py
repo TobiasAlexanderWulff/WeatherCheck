82d3d40e03486e97585020df54022a2aa030700c
TobRojekt <74661399+TobRojekt@users.noreply.github.com>
2023-04-22 03:26:40 +0200

    added Directory for Data and Date

--- FetchWeatherData.py
+++ FetchWeatherData.py
@@ -1,10 +1,15 @@
 import json
 import requests as req
 
+from datetime import date
+
 response = req.get("https://api.open-meteo.com/v1/forecast?latitude=51.67&longitude=8.34&hourly=temperature_2m,rain,showers,snowfall,cloudcover,visibility,is_day&daily=sunrise,sunset&current_weather=true&forecast_days=1&timezone=Europe%2FBerlin")
+
+directory = "WeatherData/"
+fileName = "WeatherData_" + date.today().strftime("%Y-%m-%d") + ".json"
 
 def toBeautyString(obj):
     return json.dumps(obj.json(), sort_keys=True, indent=4)
 
-with open("weatherData.json", "w") as outfile:
+with open(directory + fileName, "w") as outfile:
     outfile.write(toBeautyString(response))
