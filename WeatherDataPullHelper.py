a3a3adb37cd3d4d481f9cf93a3ab56f51aa44e2f
TobRojekt <74661399+TobRojekt@users.noreply.github.com>
2023-04-22 03:06:47 +0200

    Update WeatherDataPullHelper.py

--- WeatherDataPullHelper.py
+++ WeatherDataPullHelper.py
@@ -1,13 +1,10 @@
-import requests as req
 import json
-
-def jprint(obj):
-    # create a formatted string of the Python JSON object
-    text = json.dumps(obj, sort_keys=True, indent=4)
-    print(text)
-    
-    with open("weatherData.json", "w") as outfile:
-        outfile.write(text)
+import requests as req
 
 response = req.get("https://api.open-meteo.com/v1/forecast?latitude=51.67&longitude=8.34&hourly=temperature_2m,rain,showers,snowfall,cloudcover,visibility,is_day&daily=sunrise,sunset&current_weather=true&forecast_days=1&timezone=Europe%2FBerlin")
-jprint(response.json())
+
+def toBeautyString(obj):
+    return json.dumps(obj.json(), sort_keys=True, indent=4)
+
+with open("weatherData.json", "w") as outfile:
+    outfile.write(toBeautyString(response))
