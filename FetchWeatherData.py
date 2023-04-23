5a988acf8d7c726b58d777feaf53ad7246a3f0e2
TobRojekt <74661399+TobRojekt@users.noreply.github.com>
2023-04-23 08:14:33 +0200

    updated json naming convention
    added current hour to the name

--- FetchWeatherData.py
+++ FetchWeatherData.py
@@ -1,12 +1,12 @@
 import json
 import requests as req
 
-from datetime import date
+from datetime import datetime as dt
 
 response = req.get("https://api.open-meteo.com/v1/forecast?latitude=51.67&longitude=8.34&hourly=temperature_2m,rain,showers,snowfall,cloudcover,visibility,is_day&daily=sunrise,sunset&current_weather=true&forecast_days=1&timezone=Europe%2FBerlin")
 
 directory = "WeatherData/"
-fileName = "WeatherData_" + date.today().strftime("%Y-%m-%d") + ".json"
+fileName = "WeatherData_" + dt.now().strftime("%Y-%m-%d-%H") + ".json"
 
 def toBeautyString(obj):
     return json.dumps(obj.json(), sort_keys=True, indent=4)
