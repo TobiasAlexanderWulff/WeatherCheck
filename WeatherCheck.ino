17db85a6131bad0674b42f7bbf37fc25290c16f4
TobRojekt <74661399+TobRojekt@users.noreply.github.com>
2023-06-05 11:05:19 +0200

    added Arduino test app + updated gitignore again

---
+++ WeatherCheck.ino
@@ -0,0 +1,19 @@
+
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.println("Starting INITIALIZATION...");
+
+    
+
+
+
+
+    Serial.println("INITIALIZATION COMPLETE");
+}
+
+void loop()
+{
+    delay(1000);
+}
